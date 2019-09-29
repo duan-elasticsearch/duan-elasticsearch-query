df3971bba2ee5e2e5a152b0088a94fa9c36ce415
duan <naonao800@163.com>
2019-09-29 17:19:05 +0800

    添加wildcard模糊查询，查询没添加完，优先公司现有项目做。

--- query.go
+++ query.go
@@ -7,6 +7,7 @@ type Query struct {
 	Match interface{} `json:"match,omitempty"`
 	MatchPhrase interface{} `json:"match_phrase,omitempty"`
 	MatchAll interface{} `json:"match_all,omitempty"`
+	Wildcard interface{} `json:"wildcard,omitempty"`
 
 	Range interface{} `json:"range,omitempty"`
 }
