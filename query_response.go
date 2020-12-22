34f3b19385de5ee571a56af6eb0ed283f18e79ce
duan <naonao800@163.com>
2020-12-22 09:07:32 +0800

    转换Source到SourceObj之后，不再清除Source原始数据。

--- query_response.go
+++ query_response.go
@@ -2,55 +2,53 @@ package query
 
 import (
 	// "fmt"
-	"reflect"
 	"encoding/json"
+	"reflect"
 )
 
 type QueryResponseHitsOne struct {
-	Index string `json:"_index,omitempty" validate:"required"`
-	Type string `json:"_type,omitempty" validate:"required"`
-	Id string `json:"_id,omitempty" validate:"required"`
-	Score float64 `json:"_score,omitempty"`
-	Source json.RawMessage `json:"_source,omitempty"`
-	SourceObj interface{} `json:"_source_obj,omitempty"`
+	Index     string          `json:"_index,omitempty" validate:"required"`
+	Type      string          `json:"_type,omitempty" validate:"required"`
+	Id        string          `json:"_id,omitempty" validate:"required"`
+	Score     float64         `json:"_score,omitempty"`
+	Source    json.RawMessage `json:"_source,omitempty"`
+	SourceObj interface{}     `json:"_source_obj,omitempty"`
 }
 
 type QueryResponse struct {
-	Took int64 `json:"took,omitempty"`
-	TimedOut bool `json:"timed_out,omitempty"`
-	Shards struct {
-		Total int64 `json:"total,omitempty"`
+	Took     int64 `json:"took,omitempty"`
+	TimedOut bool  `json:"timed_out,omitempty"`
+	Shards   struct {
+		Total      int64 `json:"total,omitempty"`
 		Successful int64 `json:"successful,omitempty"`
-		Failed int64 `json:"failed,omitempty"`
+		Failed     int64 `json:"failed,omitempty"`
 	} `json:"_shards,omitempty"`
 	Hits struct {
 		Total struct {
-			Value int64 `json:"value,omitempty"`
+			Value    int64  `json:"value,omitempty"`
 			Relation string `json:"relation,omitempty"`
 		} `json:"total,omitempty"`
-		MaxScore float64 `json:"max_score,omitempty"`
-		Hits []QueryResponseHitsOne `json:"hits,omitempty"`
+		MaxScore float64                `json:"max_score,omitempty"`
+		Hits     []QueryResponseHitsOne `json:"hits,omitempty"`
 	} `json:"hits,omitempty"`
 }
 
-func (self *QueryResponse) CoverSource (demoType reflect.Type) (err error) {
+func (self *QueryResponse) CoverSource(demoType reflect.Type) (err error) {
 	for oneDataNum, oneData := range self.Hits.Hits {
-		self.Hits.Hits[oneDataNum].SourceObj = reflect.New (demoType).Interface ()
-		if err = json.Unmarshal (oneData.Source, self.Hits.Hits[oneDataNum].SourceObj); err != nil {
+		self.Hits.Hits[oneDataNum].SourceObj = reflect.New(demoType).Interface()
+		if err = json.Unmarshal(oneData.Source, self.Hits.Hits[oneDataNum].SourceObj); err != nil {
 			return
 		}
-		self.Hits.Hits[oneDataNum].Source = []byte{};
 	}
 
 	return
 }
 
-func (self *QueryResponseHitsOne) CoverHitsOne (demoType reflect.Type) (err error) {
-	self.SourceObj = reflect.New (demoType).Interface ()
-	if err = json.Unmarshal (self.Source, self.SourceObj); err != nil {
+func (self *QueryResponseHitsOne) CoverHitsOne(demoType reflect.Type) (err error) {
+	self.SourceObj = reflect.New(demoType).Interface()
+	if err = json.Unmarshal(self.Source, self.SourceObj); err != nil {
 		return
 	}
-	self.Source = []byte{};
 
 	return
 }
