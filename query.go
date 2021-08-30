259175c79cdc2fd9414797f9388dc3220313d6ed
duan <naonao800@163.com>
2021-08-30 18:33:55 +0800

    添加exists功能支持

--- query.go
+++ query.go
@@ -1,17 +1,18 @@
 package query
 
 type Query struct {
-	Bool *Bool `json:"bool,omitempty"`
-	Term interface{} `json:"term,omitempty"`
-	Terms interface{} `json:"terms,omitempty"`
-	Match interface{} `json:"match,omitempty"`
+	Bool        *Bool       `json:"bool,omitempty"`
+	Term        interface{} `json:"term,omitempty"`
+	Terms       interface{} `json:"terms,omitempty"`
+	Match       interface{} `json:"match,omitempty"`
 	MatchPhrase interface{} `json:"match_phrase,omitempty"`
-	MatchAll interface{} `json:"match_all,omitempty"`
-	Wildcard interface{} `json:"wildcard,omitempty"`
-	Regexp interface{} `json:"regexp,omitempty"`
+	MatchAll    interface{} `json:"match_all,omitempty"`
+	Wildcard    interface{} `json:"wildcard,omitempty"`
+	Exists      interface{} `json:"exists,omitempty"`
+	Regexp      interface{} `json:"regexp,omitempty"`
 
 	Range interface{} `json:"range,omitempty"`
 
 	SimpleQueryString *QueryString `json:"simple_query_string,omitempty"`
-	QueryString *QueryString `json:"query_string,omitempty"`
+	QueryString       *QueryString `json:"query_string,omitempty"`
 }
--- subnode.go
+++ subnode.go
@@ -1,13 +1,14 @@
 package query
 
 type Subnode struct {
-	Bool *Bool `json:"bool,omitempty"`
-	Term interface{} `json:"term,omitempty"`
-	Terms interface{} `json:"terms,omitempty"`
-	Match interface{} `json:"match,omitempty"`
+	Bool        *Bool       `json:"bool,omitempty"`
+	Term        interface{} `json:"term,omitempty"`
+	Terms       interface{} `json:"terms,omitempty"`
+	Match       interface{} `json:"match,omitempty"`
 	MatchPhrase interface{} `json:"match_phrase,omitempty"`
-	MatchAll interface{} `json:"match_all,omitempty"`
-	Wildcard interface{} `json:"wildcard,omitempty"`
+	MatchAll    interface{} `json:"match_all,omitempty"`
+	Wildcard    interface{} `json:"wildcard,omitempty"`
+	Exists      interface{} `json:"exists,omitempty"`
 
 	Range interface{} `json:"range,omitempty"`
 }
