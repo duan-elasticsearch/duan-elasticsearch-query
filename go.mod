b90318e8aea14453e24fc9b3361a3ddfd6f8c8d2
duan <naonao800@163.com>
2023-03-10 09:14:14 +0800

    查询子节点类型，添加对Range SimpleQueryString QueryString的支持。

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/duan-elasticsearch/duan_elasticsearch_query/v7
 
 go 1.13
-
-require github.com/elastic/go-elasticsearch/v7 v7.5.0
--- subnode.go
+++ subnode.go
@@ -9,7 +9,10 @@ type Subnode struct {
 	MatchAll    interface{} `json:"match_all,omitempty"`
 	Wildcard    interface{} `json:"wildcard,omitempty"`
 	Exists      interface{} `json:"exists,omitempty"`
+	Regexp      interface{} `json:"regexp,omitempty"`
 	Nested      interface{} `json:"nested,omitempty"`
 
-	Range interface{} `json:"range,omitempty"`
+	Range             interface{}  `json:"range,omitempty"`
+	SimpleQueryString *QueryString `json:"simple_query_string,omitempty"`
+	QueryString       *QueryString `json:"query_string,omitempty"`
 }
